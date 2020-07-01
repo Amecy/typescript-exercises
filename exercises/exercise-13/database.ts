f36b7c65b8a63618d6e7cec0a5fb50c6ce104acd
chenyao <chenyao@zycfcinc.com>
2020-07-01 16:17:12 +0800

    feat: add answers of 13~15

--- exercises/exercise-13/database.ts
+++ exercises/exercise-13/database.ts
@@ -1,13 +1,25 @@
+type Query<T> = {
+  [k in keyof T]?:
+    | { $eq: T[k] }
+    | { $lt: T[k] }
+    | { $gt: T[k] }
+    | { $in: Array<T[k]> };
+} & {
+  $text?: string;
+  $and?: Array<{ [k in keyof T]?: { [k in '$gt' | '$lt']?: number } }>;
+  $or?: Array<{ [k in keyof T]?: { [k in '$gt' | '$lt']?: number } }>;
+};
+
 export class Database<T> {
   protected filename: string;
-  protected fullTextSearchFieldNames: unknown[];
+  protected fullTextSearchFieldNames: string[];
 
-  constructor(filename: string, fullTextSearchFieldNames) {
+  constructor(filename: string, fullTextSearchFieldNames: string[]) {
     this.filename = filename;
     this.fullTextSearchFieldNames = fullTextSearchFieldNames;
   }
 
-  async find(query): Promise<T[]> {
+  async find(query: Query<T>): Promise<T[]> {
     return [];
   }
 }
--- exercises/exercise-15/database.ts
+++ exercises/exercise-15/database.ts
@@ -1,13 +1,43 @@
+type Query<T> = {
+  [k in keyof T]?:
+    | { $eq: T[k] }
+    | { $lt: T[k] }
+    | { $gt: T[k] }
+    | { $in: Array<T[k]> };
+} & {
+  $text?: string;
+  $and?: Array<{ [k in keyof T]?: { [k in '$gt' | '$lt']?: number } }>;
+  $or?: Array<{ [k in keyof T]?: { [k in '$gt' | '$lt']?: number } }>;
+};
+
+type Options<T> = {
+  sort?: { [k in keyof T]?: T[k] };
+  projection?: { [k in keyof T]?: number };
+};
+
+type Item = {
+  _id: number;
+  name: string;
+  occupation: string;
+  age: number;
+};
+
 export class Database<T> {
   protected filename: string;
-  protected fullTextSearchFieldNames: unknown[];
+  protected fullTextSearchFieldNames: string[];
 
-  constructor(filename: string, fullTextSearchFieldNames) {
+  constructor(filename: string, fullTextSearchFieldNames: string[]) {
     this.filename = filename;
     this.fullTextSearchFieldNames = fullTextSearchFieldNames;
   }
 
-  async find(query, options?): Promise<T[]> {
+  async find(query: Query<T>, options?: Options<T>): Promise<T[]> {
+    return [];
+  }
+  async delete(query: Query<T>): Promise<T[]> {
+    return [];
+  }
+  async insert(item: Item): Promise<T[]> {
     return [];
   }
 }
